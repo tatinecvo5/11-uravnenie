6444c546383b9bc2d860d9f400a2e721a56c5781
User <User@SERGEY>
2022-01-04 14:05:54 +0300

    Добавьте файлы проекта.

---
+++ 11 uravnenie/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_uravnenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Решение уравнения вида kx+b=0");
+            Console.Write("Введите k=");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите b=");
+            double b = Convert.ToDouble(Console.ReadLine());
+            /*if (k==0, b==0)
+            {
+                Console.WriteLine("Решение - любое число");
+            }
+            else 
+                if (k==0, b!==0)
+                }
+        Console.WriteLine("Решений нет");
+            }
+            else 
+    if (b!= 0)
+        {
+                                double x = -b / k;
+    Console.WriteLine("Решение: х={x}");
+        }*/
+            Uravnenie uravnenie = new Uravnenie(k, b);
+            Console.WriteLine(uravnenie.Root());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 11 uravnenie/uravnenie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_uravnenie
+{
+    struct Uravnenie
+    {
+        double k;
+        double b;
+        public Uravnenie (double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            if (k==0, b==0)
+            return "Решение - любое число";
+            if (k==0, b!==0)
+            return "Решений нет";
+            double x = -b / k;
+            return "Решение: х={x}";
+        }
+    }
+}
